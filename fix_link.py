b00e0b2a75c8658d7408c9e237d73743eb34db72
Beats <officebeats@gmail.com>
2026-02-28 11:23:06 -0600

    Update Antigravity Brain kit link to new repository

--- fix_link.py
+++ fix_link.py
@@ -3,7 +3,7 @@ with open(html_path, 'r', encoding='utf-8') as f:
     html = f.read()
 
 old_link = 'href="https://x.com/officebeats" target="_blank" rel="noopener" class="link-arrow">Follow the research &rarr;</a>'
-new_link = 'href="https://github.com/officebeats/beats-pm-antigravity-brain/blob/main/README.md#-beats-pm-antigravity-kit" target="_blank" rel="noopener" class="link-arrow">Try It Yourself, it\'s Open Source &rarr;</a>'
+new_link = 'href="https://github.com/officebeats/beats-pm-antigravity-brain" target="_blank" rel="noopener" class="btn btn-primary" style="margin-top:0.75rem;">Try It Yourself, it\'s Open Source</a>'
 
 html = html.replace(old_link, new_link)
 
